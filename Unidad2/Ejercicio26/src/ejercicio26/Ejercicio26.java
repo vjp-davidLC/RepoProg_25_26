7508f1ec7d696076c8820f1d31954acdbb21da1b
Neo <dlopezc20@informatica.iesvalledeljerteplasencia.es>
2025-10-03 16:08:09 +0200

    Ejercicio26y32
    Ejercicios

---
+++ Unidad2/Ejercicio26/src/ejercicio26/Ejercicio26.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio26;
+import java.util.Scanner;
+/**
+ *
+ * @author David
+ */
+public class Ejercicio26 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String num;
+        
+        Scanner entrada = new Scanner (System.in); // variable que recoge lo que el usuario introduzca
+        
+        System.out.println("Por favor, introduzca un numero de 4 cifras --> "); 
+        num = entrada.nextLine(); // variable que contiene el numero de 4 cifras
+        System.out.println("El numero introducido es: "+num);
+        System.out.println("La primera cifra es: "+num.charAt(0));
+        System.out.println("La primera cifra es: "+num.charAt(1));
+        System.out.println("La primera cifra es: "+num.charAt(2));
+        System.out.println("La primera cifra es: "+num.charAt(3));
+        
+    }
+    
+}
---
+++ Unidad2/Ejercicio32/src/ejercicio32/Ejercicio32.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio32;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author David
+ */
+public class Ejercicio32 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int importe;
+        int cartera;
+        
+        Scanner entrada = new Scanner (System.in);
+        
+        System.out.println("Por favor, indique una cantidad de dinero --> ");
+        importe = entrada.nextInt();
+        
+        cartera = importe;
+        int billeteCincuenta = importe / 50;
+        importe = importe - (billeteCincuenta * 50);
+        int billeteVeinte = importe / 20;
+        importe = importe - (billeteVeinte * 20);
+        int billeteDiez= importe / 10;
+        importe = importe - (billeteDiez * 10);
+        int billeteCinco= importe / 5;
+        importe = importe - (billeteCinco * 5);
+        int monedaDos = importe / 2;
+        importe = importe - (monedaDos * 2);
+        int monedaUno = importe;
+        
+        System.out.println(cartera+" Euros se descomponen en "+billeteCincuenta+" billetes de 50, "+billeteVeinte+" billetes de 20, "+billeteDiez+" billetes de 10, "
+                + ""+billeteCinco+" billetes de 5, "+monedaDos+" monedas de 2 euros y "+monedaUno+" monedas de 1 euro.");
+        
+        
+        
+        
+    }
+    
+}
