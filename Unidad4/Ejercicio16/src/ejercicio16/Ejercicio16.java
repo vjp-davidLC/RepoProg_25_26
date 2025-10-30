43a2eba11887c8361996e365189b309a6cafcfb2
Neo <dlopezc20@informatica.iesvalledeljerteplasencia.es>
2025-10-30 16:35:22 +0100

    terminar17

---
+++ Unidad4/Ejercicio16/src/ejercicio16/Ejercicio16.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio16;
+import operaciones.Operaciones;
+/**
+ *
+ * @author DAvid López
+ */
+public class Ejercicio16 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Operaciones.raizCuadrada();
+        Operaciones.ochoElevadoAlCubo();
+        Operaciones.exponencialDos();
+        Operaciones.logaritmo();
+        Operaciones.menor();
+        Operaciones.absoluto();
+        Operaciones.redondeo();
+        Operaciones.seno();
+        
+    }
+    
+}
---
+++ Unidad4/Ejercicio16/src/operaciones/Operaciones.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operaciones;
+
+/**
+ *
+ * @author alumno
+ */
+public class Operaciones {
+    
+    public static void raizCuadrada() {
+    
+        System.out.println("La raiz cuadrada de 64 es = "+Math.sqrt(64));
+    
+    }
+    
+    public static void ochoElevadoAlCubo() {
+    
+        System.out.println("8 elevado al cubo es = "+Math.pow(8, 3));
+    
+    }
+    
+    public static void exponencialDos () {
+    
+        System.out.println("Exponencial de 2 es = "+Math.exp(2));
+    
+    }
+    
+    public static void logaritmo () {
+    
+        System.out.println("Logaritmo de 2,71828 es = "+Math.log(2.71828));
+    
+    }
+    
+    public static void menor () {
+    
+        System.out.println("El menor de 2 y de 3 es = "+Math.min(2, 3));
+        
+    }
+    
+    public static void absoluto () {
+    
+        System.out.println("Valor absoluto de -4,5 es = "+Math.abs(-4.5));
+        
+    }
+    
+    public static void redondeo () {
+    
+        System.out.println("El redondeo de -4,5 con ROUND es = "+Math.round(-4.5));
+    
+    }
+    
+    public static void seno () {
+    
+        System.out.println("El seno de 45 grados es = "+Math.sin(45));
+        
+    }
+    
+}
