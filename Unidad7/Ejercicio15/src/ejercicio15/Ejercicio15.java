7c2882c3837060f0414a7614672a4af4d3b5251f
David López Castiñeira <dlopezc20@informatica.iesvalledeljerteplasencia.es>
2026-01-13 14:25:43 +0100

    terminar-15

--- Unidad7/Ejercicio15/src/ejercicio15/Ejercicio15.java
+++ Unidad7/Ejercicio15/src/ejercicio15/Ejercicio15.java
@@ -33,31 +33,34 @@ public class Ejercicio15 {
             // swicth que seleccionará el método según lo que ek usuario haya elegido
             switch (opcion) {
                 case 1:
-                    
+                    System.out.println("Rellenando automaticamente las ventas.\n");
+                    rellenarArray(ventasCoches);
                     break;
                 case 2:
-                    
+                    System.out.println("Mostrando las ventas.\n");
+                    mostrarArray(ventasCoches);
                     break;
                 case 3:
-                    
+                    System.out.println("Mostrando las ventas al reves.\n");
+                    mostrarArrayReves(ventasCoches);
                     break;
                 case 4:
-                    
+                    System.out.println("La suma total de las ventas del anho son: "+valorTotalVentas(ventasCoches)+" euros\n");
                     break;
                 case 5:
-                    
+                    System.out.println("La suma total de los meses pares son: "+valorTotalVentasPares(ventasCoches)+" euros\n");
                     break;
                 case 6:
                     
                     break;
                     
                 case 7:
-                    System.out.println("Saliendo del programa");
+                    System.out.println("Saliendo del programa...\n");
                     salir = true; // sale del programa
                     break;
                 // si el usuario no inserta un numero del 1-7 sldrá el siguiente mensaje    
                 default:
-                    System.out.println("Tiene que insertar una opcione del 1-7.");
+                    System.out.println("Tiene que insertar una opcione del 1-7.\n");
             }
             
         } while (!salir); // mientras el booleano salir no sea TRUE seguirá el bucle
@@ -79,8 +82,60 @@ public class Ejercicio15 {
     
     }
     
-    public static void rellenarArray() {
+    public static void rellenarArray(int array[]) {
     
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random()*90)+10;
+        } 
+        
+    }
+    
+    public static void mostrarArray(int array[]) {
+    
+        for (int i = 0; i < array.length; i++) {
+            
+            System.out.println("Posicion "+i+ " valor: "+array[i]+" euros");
+        }
+        System.out.println("");
+    }
+    
+    public static void mostrarArrayReves(int array[]) {
+    
+        for (int i = array.length - 1; i >= 0 ; i--) {
+            
+            System.out.println("Posicion "+i+ " valor: "+array[i]+" euros");
+        }
+        System.out.println("");
+    }
+    
+    public static int valorTotalVentas(int array[]) {
+    
+        int totalventas = 0;
+        
+        for (int i = 0; i < array.length; i++) {
+            
+            totalventas += array[i];
+            
+        }
+        
+        return totalventas;
+        
+    }
+    
+    public static int valorTotalVentasPares(int array[]) {
+    
+        String[] meses = new String[] {"Enero", "Febrero", "Marzo", "Abril","Mayo", "Junio", "Julio", "Agosto","Septiembre", "Octubre", "Noviembre", "Diciembre"};
+        
+        int totalventas = 0;
+        
+        for (int i = 1; i < array.length; i += 2) {
+            
+            totalventas += array[i];
+
+        }
+        
+        return totalventas;
+        
     }
     
 }
